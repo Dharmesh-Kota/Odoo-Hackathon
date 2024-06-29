408ec2a1c3774eb1b0f5d2fb9ea3422480f1b771
Dharmesh <dharmeshkota123@gmail.com>
2024-06-29 19:45:02 +0530

    Home page edited!

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -74,7 +74,7 @@ export default function Login() {
       .then((response) => {
         const { token, role } = response.data;
         localStorage.setItem("token", JSON.stringify(token));
-        localStorage.setItem("role", JSON.stringify(role));
+        localStorage.setItem("role", role);
         setIsLoggedIn(true);
         setRole(role);
         navigate("/");
